5ecbe2865ab4450ad3993fe295a732bd283346cb
chenukatd <chenukatd@gmail.com>
2026-06-13 18:30:58 +1000

    Add files via upload

---
+++ Kicida.lua
@@ -0,0 +1,128 @@
+---kiciada---
+    username(register)
+    password(user.set)
+while press(alt.f4)game.crashed
+    when type(/edu)education.mode(on)
+allow.user(chat)set.true
+too.much.data(game.crashed)print("sorry your game have crashed :( ")
+--education mode(math)---
+print(what the mode which is it?
+a=5
+b=7
+c=23
+d=51)
+answer=d
+    when type(got.wrong)do.it(again)
+when.type(got.correct)answer=do(option a=keep goin b=move to coding)
+--education mpode(coding)--
+print(when)
+print(do)
+print(loop)
+--codingL(loop)--
+loop=to loop something 
+loop=work
+
+when.connect(google)set.true
+--item--
+name.item(stone)
+name.item(wood)
+name.item(chicken)
+name.item(cow)
+name.item(cooblestone)
+name.item()
+--area(item)--
+stone=16x16x16
+wood=16x16x16
+chicken=12x3x2x5x2
+cow=13x3x2x5x2
+cooblestone=16x16x16
+--starting menu--
+create.button(play)
+(play)0,10,20
+create.button(join)
+(join)0,5,15
+(animation)0,2,10
+--play--
+create.world(true)
+        limit(100)
+while.reach(100)crashed.game
+--plus editon--
+buy.plus.set,cost($2)
+    limit.world(1000)
+when.promotionsend.to.user.typecode(true)
+while.promotionsend(1.month.use)
+once.player.buyplus.create.badge.name(VIP)
+--join--
+code.set(6 digt)set.true
+--device--
+moblie(touchcontrol)set.true
+pc/laptop(use.keyboard.mouse)set.true
+consle(use.controler)set.true
+--moblie--
+moblie(use.anotherediton)
+use(ME)mean=moblie.editon
+set.only(moblie.user)set.true
+use(touchcontrol)set.true
+allow.user(chat)set.true
+same.pc.but(touch)set.true
+--pc--
+allow.user(change.version)set.true.only
+allow.device.with(4GB.of.ram)
+pick.data(a,w.s,d)moves(true)
+allow.upload.mods(true)
+--mods--
+allow.user.usemods(true)
+allow.mobleuser.makemods(false)
+--account--
+age=0to4(baby.editon)
+age=5to8(kids.editon)
+age=9.above(normal.editon)
+--mobs--
+level1:chicken,cows(true)
+level:Zomble,Zombieviller(true)
+level3:ender_boss(true)
+--marketplace--
+allow.user.sell.something(false)
+    allow.user.buytosell.something(true)
+gamecurrency.name(kicicoin)
+--kicicoin--
+earn.coin(playing)
+earn.coin(buy.coins)
+cost:
+100.kicicoin:$1.99
+200.kickicoin:$5.99
+1000.kickicoin:$10.99
+2000.kicicoin:$20.99
+set(true)
+--chat-
+allow.user.say_swear_word(false)
+allow.blockedswearword(true)
+AI.replace.swear.word.with(hashtag)set.true
+--mob system--
+mobs.spawnin(8x8.area)
+ouside.mobs.spawn.in.underground(false)
+ouside.mobs.spawn.in.outside(true)
+mob.wander.anywhere(false)
+mob.wander.in.8x8.area(true)
+--animation--
+allow.user.animate(true)
+allow.user.use.mod(false)
+--dev editon--
+allow.dev.debug(true)
+allow.dev.change.code(true)
+allow.dev.invite.friends(false)
+allow.dev.use.chat(true)
+dev.item.radio.debug.tool(true)
+allow.dev.use.mods(false)
+allow.dev.test.game(true)
+game.version.at.number.and.with("Dev")badge
+--forum--
+allow.user.sumbit.bugs.updateidea(true)
+while.dev.use.forum(true)
+allow.dev.test.aoutherworld.join.without.request(true)
+--version name--
+print("0.1")
+--version name dev editon--
+print("0.1Dev")
+--dev account(list with username and password)--
+username:che.password:Thaba.7123(true)
